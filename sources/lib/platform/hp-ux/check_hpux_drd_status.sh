8dbbe6532ef7a3ecb2b5edff736dfde773de0745
Patrick Van der Veken <patrick.vanderveken@cegeka.com>
2018-05-18 11:30:59 +0200

    Stupid fix!

--- sources/lib/platform/hp-ux/check_hpux_drd_status.sh
+++ sources/lib/platform/hp-ux/check_hpux_drd_status.sh
@@ -243,7 +243,7 @@ else
     _MSG="unable to run command: {${_DRD_BIN}}"
     log_hc "$0" 1 "${_MSG}"
     # dump debug info
-    (( ARG_DEBUG != 0 && ARG_DEBUG_LEVEL = 2 ))
+    if (( ARG_DEBUG != 0 && ARG_DEBUG_LEVEL > 0 ))
     then
         log "$(<${HC_STDOUT_LOG})"
         log "$(<${HC_STDERR_LOG})"
